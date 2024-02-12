7ed4574bbf86c4d6be1a28acc2e96871a0028c97
Brad Balmer <bbalmer@laughlin.com>
2024-02-11 19:31:27 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,10 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | Brutals Character Dashboard',
+    default: 'Brutals Character Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  description: 'The official Brutals app for Kromotas mods.',
 };
 
 export default function RootLayout({
