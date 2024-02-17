e2504835c1cdbebad136fe69567d3f95d5c4adbf
Brad Balmer <bbalmer@laughlin.com>
2024-02-17 09:14:33 -0600

    Update page.tsx

--- app/dashboard/edit/refresh/page.tsx
+++ app/dashboard/edit/refresh/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
     return (
         <main>
 
-            <button onClick={handleClick} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Refresh Swgoh Characters</button>
+            <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Refresh Swgoh Characters</button>
 
         </main>
     );
